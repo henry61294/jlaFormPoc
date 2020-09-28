845b4a4ad02c4306b4d2c18a76cb03a80ad39208
shaajali786 <60658619+shaajali786@users.noreply.github.com>
2020-09-28 14:51:31 +0100

    created form on .html and component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactServicingRepairsComponent } from './contact-servicing-repairs/contact-servicing-repairs.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:ContactServicingRepairsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
